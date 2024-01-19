b39712522ed52881d873fbb705282b8bef74c07a
JiHyeong23 <116024567+JiHyeong23@users.noreply.github.com>
2024-01-19 11:01:26 +0900

    Time: 42 ms (93.21%), Space: 17.7 MB (18.01%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,6 +1,18 @@
+import bisect
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ans = []
+        if not nums1 or not nums2:
+            return []
+        
+        ans = set()
+        nums2.sort()
+        for i in nums1:
+            idx = bisect.bisect_left(nums2, i)
+            if len(nums2) > idx and i == nums2[idx]:
+                ans.add(i)
+        return list(ans)
+    
+        '''ans = []
         if len(nums1) > len(nums2):
             for i in nums2:
                 if i in nums1:
@@ -16,3 +28,4 @@ class Solution:
         
         ans = list(set(ans))
         return ans
+        '''
